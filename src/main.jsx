313f56597c812e158f91609e7dc414e947875c0a
vishalsharma847 <vishalsharma84879@gmail.com>
2024-09-13 12:44:52 +0530

    ref changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <BrowserRouter basename={import.meta.env.BASE_URL}> */}
+    <BrowserRouter basename='/vite-deploy/'>
+    <Routes>
+     <Route path="/" element={<App />}/>
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
