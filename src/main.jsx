71c29448b499089ed8b03135af7f946cceaf786d
vishalsharma847 <vishalsharma84879@gmail.com>
2024-09-13 12:47:20 +0530

    second try

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <BrowserRouter basename={import.meta.env.BASE_URL}> */}
-    <BrowserRouter basename='/vite-deploy/'>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
     <Routes>
      <Route path="/" element={<App />}/>
     </Routes>
